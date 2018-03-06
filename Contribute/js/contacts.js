a1345e09df4597dd6252a8d9c828376b77b84980
校建强 <isjianqiang@outlook.com>
2018-03-06 15:19:13 +0800

    copy

---
+++ Contribute/js/contacts.js
@@ -0,0 +1,8 @@
+function showWeixin(value) {
+    if( !value ) {
+        $("#wxImg-container").css("display","none");
+    }
+    else {
+        $("#wxImg-container").css("display","inline");
+    }
+}
--- Contribute/to/js/app.js
+++
@@ -1,5 +0,0 @@
-$(function(){
-	$('.drop-arrow-button').on('click',function(){
-		$(this).toggleClass('down').parents('.collapse_content').find('.collapse').slideToggle('fast');
-	});
-})
